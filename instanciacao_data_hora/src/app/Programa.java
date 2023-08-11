de7bb1d3c35d7f98f2e3291eb36fe6feb8b0d717
Guiideed <guilhermenovolg@gmail.com>
2023-08-10 23:55:47 -0300

    Instanciacao de dataTime

--- instanciacao_data_hora/src/app/Programa.java
+++ instanciacao_data_hora/src/app/Programa.java
@@ -5,33 +5,57 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Programa {
-	public static void main(String[ ]args) {
-		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+public class Programa{
+	public static void main(String[]args) {
 		
-		LocalDate d01 = LocalDate.now(); //data local;
 		
-		LocalDateTime d02 = LocalDateTime.now();//Horario local
+		//imprimir data de agora
+		LocalDate d01 = LocalDate.now();	
 		
-		Instant d03 = Instant.now();//GNT horario de londres
+		//imprimir data e hora de agora
+		LocalDateTime d02 = LocalDateTime.now();
 		
+		//imprimir data e hora global GMT +3(Londres)
+		Instant d03 = Instant.now();
+		
+		//Texto ISO 8601 para Data
 		LocalDate d04 = LocalDate.parse("2022-07-20");
 		
+		//Texto ISO 8601 para DataHora
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		
+		//Texto ISO 8601 para DataHoraGlobal
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+		 
+		//Texto ISO 8601 para DataHoraGlobal definindo um fuso horario
+		Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
 		
-		LocalDate d07 = LocalDate.parse("20/07/2022", fmt1);
+		//Texto customizado usando o DateTimeFormatter
+		LocalDate d08 = LocalDate.parse("20/07/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		
-		LocalDate d08 = LocalDate.parse("20/07/2022 01:30", fmt2);
-		System.out.println("d01 = " + d01);
-		System.out.println("d02 = " + d02);
-		System.out.println("d03 = " + d03);
-		System.out.println("d04 = " + d04);
-		System.out.println("d05 = " + d05);
-		System.out.println("d06 = " + d06);
-		System.out.println("d07 = " + d07);
-		System.out.println("d08 = " + d08);
+		//Texto customizado usando o DateTimeFormatter com horas também
+		LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+		
+		//dia, mes e ano para data
+		LocalDate d10 = LocalDate.of(2022, 7, 20);
+		
+		//dia, mes ano e hora para DateTime
+		LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 1,30);
+		
+		
+		
+		
+		System.out.println("d01 = "+ d01);
+		System.out.println("d02 = "+ d02);
+		System.out.println("d03 = "+ d03);
+		System.out.println("d04 = "+ d04);
+		System.out.println("d05 = "+ d05);
+		System.out.println("d06 = "+ d06);
+		System.out.println("d07 = "+ d07);
+		System.out.println("d08 = "+ d08);
+		System.out.println("d09 = "+ d09);
+		System.out.println("d10 = "+ d10);
+		System.out.println("d11 = "+ d11);
 	}
+	
 }
